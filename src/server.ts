7dd7a5ae7e3d322b744287925322ff81ac9768ff
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-15 10:53:28 -0300

    feat: iniciando server

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
-import Fastify from "fastify";
-import fastifyCors from "@fastify/cors";
+import fastify from "fastify";
 
-const app = Fastify({
-     logger:true
+const app = fastify()
+
+app.get('/',async ()=>{
+    return "hello Wold"
 })
 
-const start  = async () =>{
-    try{
-        await app.listen({
-            port:3333
-        })
-    }catch(err){
-        process.exit(1)
-    }
-}
-start()
+app.listen({
+    port: 3333,
+}).then(()=>{
+    console.log('HTTP Server Running!')
+})
+// npm install -D @types/node pra usar o ts com node e necessario esse comando pra gerar o arquivo na versao js
+// npm install tsx -D  faz todo processo de converter ts for js e  executar sem gerar arquivo js na pasta  src 
+// npx tsx src/server.ts basicamente usa o  arquivo ts diretamente
+// importante nao esquecer de colocar o watch dps do tsx pra assistir modificacaoes executar
+//tsx watch src/server.ts nao e necessario colocar  npx no script
