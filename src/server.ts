8f13802a87a5b389caaa3f2bd2527e5f06fbfcda
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-15 11:53:21 -0300

    feat: instalando e configurando eslint

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,20 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
-const app = fastify()
+const app = fastify();
 
 app.get('/',async ()=>{
-    return "hello Wold"
-})
+	return 'hello Wold';
+});
 
 app.listen({
-    port: 3333,
+	port: 3333,
 }).then(()=>{
-    console.log('HTTP Server Running!')
-})
+	console.log('HTTP Server Running!');
+});
 // npm install -D @types/node pra usar o ts com node e necessario esse comando pra gerar o arquivo na versao js
 // npm install tsx -D  faz todo processo de converter ts for js e  executar sem gerar arquivo js na pasta  src 
 // npx tsx src/server.ts basicamente usa o  arquivo ts diretamente
 // importante nao esquecer de colocar o watch dps do tsx pra assistir modificacaoes executar
 //tsx watch src/server.ts nao e necessario colocar  npx no script
+
+// npm i eslint -D  instalando eslint como dependencia de desenvolvimento
