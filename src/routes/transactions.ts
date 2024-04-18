290a95fc948baa4b1cfa60bb6829795a9187f7ad
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-18 10:30:22 -0300

    feat: rota de criated transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify';
+import knex from 'knex';
 import { z } from 'zod';
 
 export async function transactionsRoutes(app:FastifyInstance){
@@ -10,6 +11,15 @@ export async function transactionsRoutes(app:FastifyInstance){
 			amount:z.number(),
 			type:z.enum(['credit', 'debit']),
 		});
+		const {title,amount,type} = createTransactionsBodySchema.parse(request.body);
+
+		await knex('transactions').insert({
+			id: crypto.randomUUID(),
+			title,
+			amount: type === 'credit' ?  amount : amount * -1,
+		});
+		return reply.status(201).send();
+		//  201 recurso criado com sucesso
 	})
 	
 	;
