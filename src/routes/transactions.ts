f70be3e86885e7220709e908b4e2b0581684feef
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-18 17:32:05 -0300

    feat: create rota de listar all transactions and list transaction for id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,9 +3,22 @@ import { z } from 'zod';
 import { knex } from '../database';
 
 export async function transactionsRoutes(app:FastifyInstance){
+	app.get('/', async ()=>{
+		const transactions =  await knex('transactions').select();
+		return {transactions};
+	});
+
+	app.get('/:id', async (request)=>{
+		const getTransactionsParamsSchema = z.object({
+			id:z.string().uuid(),
+		});
+
+		const { id } = getTransactionsParamsSchema.parse(request.params);
+		const transaction = await knex('transactions').where('id', id).first();
+		return { transaction };
+	});
+
 	app.post('/', async (request, reply)=>{
-
-
 		const createTransactionsBodySchema= z.object({
 			title: z.string(),
 			amount:z.number(),
@@ -18,7 +31,7 @@ export async function transactionsRoutes(app:FastifyInstance){
 			title,
 			amount: type === 'credit' ?  amount : amount * -1,
 		});
-		return reply.status(201).send();
+		return reply.status(201).send('paozinho de alho');
 		//  201 recurso criado com sucesso
 	})
 	
