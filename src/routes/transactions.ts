f0d7e1837011bcd52b44ba7a9200fe98b1eaeac9
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-18 10:58:44 -0300

    fixing: import knex

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify';
-import knex from 'knex';
 import { z } from 'zod';
+import { knex } from '../database';
 
 export async function transactionsRoutes(app:FastifyInstance){
 	app.post('/', async (request, reply)=>{
